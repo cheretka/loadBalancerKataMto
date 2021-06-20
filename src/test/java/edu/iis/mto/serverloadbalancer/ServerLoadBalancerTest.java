e4b4eb4d9ae77144f9c935886edf07d2ecf7996d
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-06-20 21:14:09 +0300

    1.3 refactor

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -1,53 +1,42 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
+import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
-import org.hamcrest.Matcher;
 import org.junit.Test;
 
-import javax.management.MBeanServerBuilder;
 
 public class ServerLoadBalancerTest {
-	@Test
-	public void itCompiles() {
-		assertThat(true, equalTo(true));
-	}
+    @Test
+    public void itCompiles() {
+        assertThat(true, equalTo(true));
+    }
 
-	@Test
-	public void balancingaServer_noVms_serverStaysEmpty() {
-		Server theServer = a(server().withCapacity(1));
-		balance(aListOfServersWith(theServer), anEmptyListOfVms());
-		assertThat(theServer, hasLoadPercentageOf(0.0d));
-	}
+    @Test
+    public void balancingAServer_noVms_serverStaysEmpty() {
+        Server theServer = a(server().withCapacity(1));
 
+        balance(aListOfServersWith(theServer), anEmptyListOfVms());
 
-	private Matcher<? super Server> hasLoadPercentageOf(double expectedLoadPercentage) {
-		return new CurrentLoadPercentageMatcher(expectedLoadPercentage);
-	}
+        assertThat(theServer, hasLoadPercentageOf(0.0d));
+    }
 
-	private void balance(Server[] servers, Vm[] vms) {
-		new ServerLoadBalancer().balance(servers, vms);
-	}
+    private void balance(Server[] servers, Vm[] vms) {
+        new ServerLoadBalancer().balance(servers, vms);
+    }
 
-	private Vm[] anEmptyListOfVms() {
-		return new Vm[0];
-	}
+    private Vm[] anEmptyListOfVms() {
+        return new Vm[0];
+    }
 
+    private Server[] aListOfServersWith(Server server) {
+        return new Server[]{server};
+    }
 
-	private Server[] aListOfServersWith(Server server) {
-		return new Server[]{server};
-	}
-
-
-	private Server a(ServerBuilder builder) {
-		return builder.build();
-	}
-
-
-	private ServerBuilder server() {
-		return new ServerBuilder();
-	}
-
+    private Server a(ServerBuilder builder) {
+        return builder.build();
+    }
 }
