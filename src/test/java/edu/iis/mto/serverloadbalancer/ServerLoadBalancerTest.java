0ffa2a42b56e2f3ef48838af05ede634e6d7cb8a
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-06-20 21:52:14 +0300

    3.1 red

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -37,6 +37,18 @@ public class ServerLoadBalancerTest {
         assertThat("the server should contain vm", theServer.contains(theVm));
     }
 
+    @Test
+    public void balancingOneServerWithTenSlotsCapacity_andOneSlotVm_fillTheServerWithTenPercent(){
+        Server theServer = a(server().withCapacity(10));
+        Vm theVm = a(vm().ofSize(1));
+        balance(aListOfServersWith(theServer), aListOfVmsWith(theVm));
+
+        assertThat(theServer, hasLoadPercentageOf(10.0d));
+        assertThat("the server should contain vm", theServer.contains(theVm));
+
+    }
+
+
     private void balance(Server[] servers, Vm[] vms) {
         new ServerLoadBalancer().balance(servers, vms);
     }
@@ -55,4 +67,5 @@ public class ServerLoadBalancerTest {
     private <T> T a(Builder<T> builder) {
         return builder.build();
     }
+
 }
