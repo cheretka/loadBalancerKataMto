967761155dc85aa4d57a77d8703498b53b1e0577
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-06-21 07:40:43 +0300

    test 6 refactor

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -20,7 +20,11 @@ public class Server {
 
 	public void addVm(Vm vm) {
 		this.vms.add(vm);
-		this.currentLoadPecentage = (double) vm.size / (double) this.capacity
+		this.currentLoadPecentage = getLoad(vm);
+	}
+
+	private double getLoad(Vm vm) {
+		return (double) vm.size / (double) this.capacity
 				* MAXIMUM_LOAD;
 	}
 
@@ -33,7 +37,6 @@ public class Server {
 	}
 
 	public boolean canFit(Vm vm) {
-		return currentLoadPecentage + ((double) vm.size / (double) this.capacity
-				* MAXIMUM_LOAD) <= MAXIMUM_LOAD;
+		return currentLoadPecentage + (getLoad(vm)) <= MAXIMUM_LOAD;
 	}
 }
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -7,17 +7,26 @@ public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			List<Server> serversThatCanFitVm = new ArrayList<>();
-			for(Server server:servers){
-				if(server.canFit(vm))
-					serversThatCanFitVm.add(server);
-			}
-
-			Server lessLoaded = extractLessLoadedServer(serversThatCanFitVm);
-			if(lessLoaded!=null)
-				lessLoaded.addVm(vm);
+			addToLessLoadedServer(servers, vm);
 		}
 
+	}
+
+	private void addToLessLoadedServer(Server[] servers, Vm vm) {
+		List<Server> serversThatCanFitVm = getServersWithEnoughCapacity(servers, vm);
+
+		Server lessLoaded = extractLessLoadedServer(serversThatCanFitVm);
+		if(lessLoaded!=null)
+			lessLoaded.addVm(vm);
+	}
+
+	private List<Server> getServersWithEnoughCapacity(Server[] servers, Vm vm) {
+		List<Server> serversThatCanFitVm = new ArrayList<>();
+		for(Server server: servers){
+			if(server.canFit(vm))
+				serversThatCanFitVm.add(server);
+		}
+		return serversThatCanFitVm;
 	}
 
 	private Server extractLessLoadedServer(List<Server> servers) {
