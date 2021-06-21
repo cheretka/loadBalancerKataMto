ddbaffb6105fab4e3ac7097244dfc12a927c0eb7
Ocheretian.Kateryna <226448@edu.p.lodz.pl>
2021-06-21 06:43:05 +0300

    test 5 green

--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -4,7 +4,13 @@ public class ServerLoadBalancer {
 
 	public void balance(Server[] servers, Vm[] vms) {
 		for (Vm vm : vms) {
-			servers[0].addVm(vm);
+			Server lessLoadedServer = null;
+			for(Server s: servers){
+				if(lessLoadedServer==null||(s.currentLoadPecentage<lessLoadedServer.currentLoadPecentage)){
+					lessLoadedServer = s;
+				}
+			}
+			lessLoadedServer.addVm(vm);
 		}
 
 	}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -3,6 +3,7 @@ package edu.iis.mto.serverloadbalancer;
 public class ServerBuilder implements Builder<Server> {
 
 	private int capacity;
+	private double loadPercentage;
 
 	public ServerBuilder withCapacity(int capacity) {
 		this.capacity = capacity;
@@ -10,10 +11,22 @@ public class ServerBuilder implements Builder<Server> {
 	}
 
 	public Server build() {
+		Server server = new Server(capacity);
+
+		if (loadPercentage>0.0d){
+			int load = (int)(loadPercentage/100.0d * server.getCapacity());
+			server.addVm(VmBuilder.vm().ofSize(load).build());
+		}
+
 		return new Server(capacity);
 	}
 
 	public static ServerBuilder server() {
 		return new ServerBuilder();
+	}
+
+	public ServerBuilder withCurrentLoadOf(double loadPerc) {
+		this.loadPercentage = loadPerc;
+		return this;
 	}
 }
